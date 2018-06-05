5fa16937fd1b7fc705bfd5aa1f84c126379f0656
hemejef <account2707@hotmail.com>
2018-06-04 21:51:18 -0400

    Created Main Camera and Player

---
+++ StoreRiot/Assets/_Scripts/MoveCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+    [SerializeField]
+    float distanceFromPlayer;
+    Transform playerTr;
+	// Use this for initialization
+	void Start () {
+        playerTr = GameObject.FindGameObjectWithTag("Player").transform;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3 (playerTr.position.x, playerTr.position.y + distanceFromPlayer, playerTr.position.z);
+	}
+}
---
+++ StoreRiot/Assets/_Scripts/MovePlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    [SerializeField] float walkSpeed, runSpeed;
+    float speed = 0;
+    Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        speed = walkSpeed;
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = walkSpeed;
+        }
+        Vector3 v = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        rb.velocity = v * speed;
+        transform.right = rb.velocity;
+    }
+}
